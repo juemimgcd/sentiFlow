7979a41c3a585cfdfdeec2a283799a233cb4aca4
juemimgcd <3489161689@qq.com>
2026-05-06 13:06:28 +0800

    update the router

--- router/health.py
+++ router/health.py
@@ -7,7 +7,8 @@ router = APIRouter(prefix="/health", tags=["health"])
 
 @router.get("")
 def health_check():
-
+    # 健康检查接口一般不做复杂逻辑，它的主要任务是告诉你：
+    # 服务是否启动了，路由是否注册成功了。
     return success_response(
         data={"service": "agentic-rag", "status": "running"},
         message="service is healthy",
